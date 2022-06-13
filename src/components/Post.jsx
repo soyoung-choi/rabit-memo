ba40b7ee24fea5fe61fec1d75c3850311943a94b
Soyoung Choi <sydesign24@naver.com>
2022-06-13 15:13:50 +0900

    Update Post.jsx

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,10 +1,10 @@
 const Post = ({ postObj }) => {
   return (
-    <>
+    <div className="post-card">
       <h3>{postObj.title}</h3>
       <p>{postObj.contents}</p>
-    </>
-  )
-}
+    </div>
+  );
+};
 
-export default Post
+export default Post;
